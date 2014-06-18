8e676ccf60b989264b1a24e59c0de9d1315c6cac
nrock <nmacellaio@gmail.com>
2014-06-18 11:38:46 -0500

    added project with mongo

---
+++ Source/MongoCms.Web/App_Start/RealEstateContext.cs
@@ -0,0 +1,33 @@
+﻿namespace RealEstate.App_Start
+{
+	using MongoDB.Driver;
+    using MongoCms.Properties;
+    //using Rentals;
+
+    public class MongoCmsContext
+	{
+		public MongoDatabase Database;
+
+		public MongoCmsContext()
+		{
+			var client = new MongoClient(Settings.Default.ConnectionString);
+			var server = client.GetServer();
+            Database = server.GetDatabase(Settings.Default.MongoCmsDatabaseName);
+		}
+
+        //public MongoCollection<Rental> Rentals
+        //{
+        //    get
+        //    {
+        //        return Database.GetCollection<Rental>("rentals");
+        //    }
+        //}
+	}
+}
+
+
+
+
+
+
+
---
+++ Source/MongoCms.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using RealEstate.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MongoCms.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public MongoCmsContext _context = new MongoCmsContext();
+
+        public ActionResult Info()
+        {
+            _context.Database.GetStats();
+            return Json(_context.Database.Server.BuildInfo, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
